69fb2d2af557accb246d878e75e85e56815ba85b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-03-25 23:49:59 -0400

    Greatly simplify code
    1. Use re-search-forward instead of splitting lines etc.
    
    2. Use mapcar with seq-partition on the match data to obtain matched
    substrings.
    
    3. Remove verbose defvar definitions.
    
    4. Move main code into a function that accepts the speed parameter as
    an argument.

--- xinput.el
+++ xinput.el
@@ -1,43 +1,30 @@
 ;; -*- lexical-binding: t -*-
 
-(defvar *bci/touchpad-name* "SynPS/2 Synaptics TouchPad"
-  "The name of my Thinkpad X201 touchpad, found by inspecting the
-  output of 'xinput list'")
+(defvar *touchpad-id-regexp*
+  (format "%s%s"
+          "SynPS/2 Synaptics TouchPad"
+          "[[:space:]]+id=\\([[:digit:]]+\\)"))
 
-(defvar *bci/touchpad-property-name* "Accel Speed"
-  "The property associated with my touchpad's cursor speed.")
+(defvar *touchpad-property-regexp*
+  (format "%s%s"
+          "Accel Speed"
+          "[[:space:]](\\([[:digit:]]+\\))"))
 
-(defvar *bci/touchpad-speed* "1"
-  "The desired touchpad speed.")
-
-(defun bci/xinput-find-datum (commands regexp)
-  "Return all match-strings associated with finding REGEXP among
-the output lines of the given xinput invocation (whose input is
-given as a list of COMMANDS, e.g. (list \"list-props\" \"11\").
-
-Usually, we'll only be interested in a single match-string >= 1,
-though in theory we should be allowed to use regexps with as many
-regexp groups as we desire; hence we return MATCHES as a list of
-all such groups, including the entire match itself (index 0)"
+(defun xinput-find-match-data (regexp &rest commands)
   (with-temp-buffer
-    ;; Insert output of xinput invocation into this buffer
     (apply #'call-process "xinput" nil t nil commands)
-    (let* ((output-lines (split-string (buffer-string) "\n"))
-           (target-line (seq-find
-                         (lambda (line) (string-match regexp line))
-                         output-lines)))
-      (let ((index 0)
-            (matches nil)
-            (current-match nil))
-        (while (setq current-match (match-string index target-line))
-          (push (match-string index target-line) matches)
-          (cl-incf index))
-        matches))))
+    (beginning-of-buffer)
+    (re-search-forward regexp)
+    (mapcar (lambda (pair) (apply #'buffer-substring-no-properties pair))
+            (seq-partition (butlast (match-data 'integers)) 2))))
+
+(defun xinput-set-touchpad-speed (speed)
+  (let* ((device-id
+          (nth 1 (xinput-find-match-data *touchpad-id-regexp* "list")))
+         (device-property
+          (nth 1 (xinput-find-match-data *touchpad-property-regexp* "list-props" device-id))))
+    (call-process "xinput" nil nil nil "set-prop" device-id device-property speed)))
 
 ;; Let 'er rip!  This should fix our touchpad speed every time we
 ;; start Emacs, which is every time we launch an X session.
-(let* ((device-id
-        (car (bci/xinput-find-datum (list "list") (concat *bci/touchpad-name*  "[[:space:]]+id=\\([[:digit:]]+\\)"))))
-       (device-property
-        (car (bci/xinput-find-datum (list "list-props" device-id) (concat *bci/touchpad-property-name* "[[:space:]](\\([[:digit:]]+\\))")))))
-  (call-process "xinput" nil nil nil "set-prop" device-id device-property "1"))
+(xinput-set-touchpad-speed "1")
